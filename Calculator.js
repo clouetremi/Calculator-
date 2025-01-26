9ac0a3c3e8fe92bd12ea7f43e50208ce24464d49
Rémi Clouet <clouet.remi@gmail.com>
2025-01-26 17:04:57 +0100

    Fin de la structure avec les calculs de base du code

--- Calculator.js
+++ Calculator.js
@@ -27,18 +27,61 @@ console.log(divide(15, 5));
 
 let a = 15;
 let b = 15;
-let operator = "-";
+let operator = "";
 
-function operate(a, b, operator){
-    if (operator === "+"){
-        return addition(a, b)
-    } else if (operator === "-"){
-        return subtract(a, b)
-    } else if (operator === "*"){
-        return multiply(a, b)
-    } else if (operator === "/"){
-        return divide(a, b);
-    };
+function operate(a, b, operator) {
+    switch (operator) {
+        case "+":
+            result = addition(a, b);
+            break;
+        case "-":
+            result = subtract(a, b);
+            break;
+        case "*":
+            result = multiply(a, b);
+            break;
+        case "/":
+            result = divide(a, b);
+        default: console.log("Opérateur non valide")
+    }
+    return result;
 };
 
-console.log(`Le résultat est de ${operate(a, b, operator)}`)
+// Create the functions that populate the display when you click the digit buttons. 
+// You should store the content of the display (the number) in a variable for use in the next step.
+
+const buttons = document.querySelectorAll("button");
+let storeOne = "";
+let storeTwo = "";
+let isOperatorSelected = false; // Pour savoir si un opérateur a été choisi
+
+buttons.forEach((bouton) => {
+    bouton.addEventListener("click", () => {
+
+        const value = bouton.innerText;
+
+        if (value === "+" || value === "-" || value === "*" || value === "/") {
+            operator = value;
+            isOperatorSelected = true;
+            console.log(`Opérateur choisi ${operator}`)
+        } else if (value === "=") {
+            if (storeOne && storeTwo && isOperatorSelected) {
+                a = parseFloat(storeOne);
+                b = parseFloat(storeTwo);
+                result = operate(a, b, operator);
+                console.log(`Result is ${result}`);
+            }
+            else {
+                console.log("Opérateur invalide !")
+            }
+        } else {
+            if (!isOperatorSelected){
+                storeOne += value; 
+                console.log(`StoreOne : ${storeOne}`)
+            } else {
+                storeTwo += value; 
+                console.log(`StoreTwo : ${storeTwo}`)
+            }
+        }
+    });
+});
