2ae31452dbd69de6ba4b2aa9e8ac1fcb7c292cec
Rémi Clouet <clouet.remi@gmail.com>
2025-01-27 22:09:58 +0100

    Ajout du code pour le display de notre selection

--- Calculator.js
+++ Calculator.js
@@ -2,31 +2,28 @@
 const addition = function (a, b) {
     return a + b;
 };
-console.log(addition(5, 6));
 
 // subtract
 const subtract = function (a, b) {
     return a - b;
 };
-console.log(subtract(10, 15));
+
 // multiply
 const multiply = function (a, b) {
     return a * b;
 };
-console.log(multiply(10, 10));
 
 // divide
 const divide = function (a, b) {
     return a / b;
 };
-console.log(divide(15, 5));
 
 // A calculator operation will consist of a number, an operator, and another number. 
 // For example, 3 + 5. Create three variables, one for each part of the operation. 
 // You’ll use these variables to update your display later.
 
-let a = 15;
-let b = 15;
+let a = "";
+let b = "";
 let operator = "";
 
 function operate(a, b, operator) {
@@ -42,46 +39,72 @@ function operate(a, b, operator) {
             break;
         case "/":
             result = divide(a, b);
-        default: console.log("Opérateur non valide")
-    }
+            break;
+        default: "OOPS"
+    };
     return result;
 };
 
 // Create the functions that populate the display when you click the digit buttons. 
 // You should store the content of the display (the number) in a variable for use in the next step.
 
+const displaySum = document.getElementById("displaySum");
+displaySum.innerText = "Faites votre opération, exemple : 3 + 3";
+
 const buttons = document.querySelectorAll("button");
-let storeOne = "";
-let storeTwo = "";
-let isOperatorSelected = false; // Pour savoir si un opérateur a été choisi
 
 buttons.forEach((bouton) => {
     bouton.addEventListener("click", () => {
 
         const value = bouton.innerText;
+        let valueDisplayed = "";
+        valueDisplayed += value;
+        displaySum.innerText = valueDisplayed;
 
-        if (value === "+" || value === "-" || value === "*" || value === "/") {
-            operator = value;
-            isOperatorSelected = true;
-            console.log(`Opérateur choisi ${operator}`)
-        } else if (value === "=") {
-            if (storeOne && storeTwo && isOperatorSelected) {
-                a = parseFloat(storeOne);
-                b = parseFloat(storeTwo);
-                result = operate(a, b, operator);
-                console.log(`Result is ${result}`);
-            }
-            else {
-                console.log("Opérateur invalide !")
-            }
-        } else {
-            if (!isOperatorSelected){
-                storeOne += value; 
-                console.log(`StoreOne : ${storeOne}`)
-            } else {
-                storeTwo += value; 
-                console.log(`StoreTwo : ${storeTwo}`)
-            }
-        }
+
     });
 });
+
+
+
+// Créer une varialbe pour sélectionner le bouton afin de l'appeler avec un eventListener
+// const buttons = document.querySelectorAll("button");
+// let storeOne = "";
+// let storeTwo = "";
+// let isOperatorSelected = false;
+// let isSecondOperatorSelected = false;
+
+
+// buttons.forEach((bouton) => {
+//     bouton.addEventListener("click", () => {
+
+//         const value = bouton.innerText;
+
+//         if (value === "=") {
+//             if (storeOne && storeTwo && isOperatorSelected) {
+//                 a = parseFloat(storeOne);
+//                 b = parseFloat(storeTwo);
+//                 const result = operate(a, b, operator);
+//                 console.log(`Le résultat est ${result}`)
+//             } else {
+//                 console.log("Erreur, veuillez refaire votre opération")
+//             }
+//         } else if (value === "Clear") {
+//             storeOne = "";
+//             storeTwo = "";
+//             isOperatorSelected = false;
+//             console.log("Reset ok, tout est bien à 0")
+//         } else if (value === "+" || value === "-" || value === "*" || value === "/") {
+//             isOperatorSelected = true;
+//             operator = value;
+//             console.log(`Operator selected is ${operator}`)
+//         } else if (!isOperatorSelected) {
+//             storeOne += value;
+//             console.log(`La première valeur selectionnée est ${storeOne}`)
+//         } else {
+//             storeTwo += value;
+//             console.log(`La seconde valeur selectionnée est ${storeTwo}`)
+//         }
+//     })
+// })
+
