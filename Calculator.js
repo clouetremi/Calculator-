b4714203247c2b89d99184b673088156a46433f4
Rémi Clouet <clouet.remi@gmail.com>
2025-01-29 17:13:53 +0100

    code complet

--- Calculator.js
+++ Calculator.js
@@ -42,69 +42,88 @@ function operate(a, b, operator) {
             break;
         default: "OOPS"
     };
-    return result;
+    return parseFloat(result.toFixed(2)); // Méthode toFixed(x) pour arrondir les décimals au nombre x 
 };
 
 // Create the functions that populate the display when you click the digit buttons. 
 // You should store the content of the display (the number) in a variable for use in the next step.
 
+const buttons = document.querySelectorAll("button");
 const displaySum = document.getElementById("displaySum");
 displaySum.innerText = "Faites votre opération, exemple : 3 + 3";
+let valueDisplayed = "";
 
-const buttons = document.querySelectorAll("button");
+
+let storeOne = "";
+let storeTwo = "";
+let isOperatorSelected = false;
 
 buttons.forEach((bouton) => {
     bouton.addEventListener("click", () => {
 
         const value = bouton.innerText;
-        let valueDisplayed = "";
         valueDisplayed += value;
         displaySum.innerText = valueDisplayed;
+        let justCalculated = false; // Variable pour activer le display de l'opérateur suivant s'il est choisi avant storeOne ou après storeTwo 
 
 
+        if (value === "=") {
+            if (storeOne && storeTwo && isOperatorSelected) {
+                if (storeTwo === "0" && operator === "/") {
+                    displaySum.innerText = "Division par 0, lol ?";
+                    storeOne = "";
+                    storeTwo = "";
+                    isOperatorSelected = false;
+                } else {
+                    a = parseFloat(storeOne);
+                    b = parseFloat(storeTwo);
+                    const result = operate(a, b, operator);
+                    console.log(`Le résultat est ${result}`)
+                    displaySum.innerText = result;
+                }
+            } else {
+                console.log("Erreur, veuillez refaire votre opération");
+                displaySum.innerText = "Erreur, veuillez refaire votre opération";
+            }
+        } else if (value === "Clear") {
+            storeOne = "";
+            storeTwo = "";
+            isOperatorSelected = false;
+            console.log("Reset ok, tout est bien à 0");
+            displaySum.innerText = "0";
+        } else if (value === "+" || value === "-" || value === "*" || value === "/") {
+            // Lance l'opération dans le cas où on choisis un opérateur une deuxième fois au lieu de choisir "="
+            if (storeOne && storeTwo && isOperatorSelected) {
+                a = parseFloat(storeOne);
+                b = parseFloat(storeTwo);
+                const result = operate(a, b, operator);
+                console.log(`Le résultat est ${result}`)
+                displaySum.innerText = result;
+
+                // Réinitialise storeOne avec le résultat pour permettre une nouvelle opération
+                storeOne = result.toString();
+                storeTwo = "";
+                justCalculated = true; // Indique qu'un opération vient d'être effectuée
+            }
+
+            // Si une opération vient d'être effectuée, ne met pas à jour l'affichage avec l'opérateur
+            if (!justCalculated) {
+                displaySum.innerText = value;
+            }
+
+            isOperatorSelected = true;
+            operator = value;
+            console.log(`Operator selected is ${operator}`);
+            justCalculated = false;
+        } else if (!isOperatorSelected) {
+            storeOne += value;
+            console.log(`La première valeur selectionnée est ${storeOne}`);
+            displaySum.innerText = storeOne;
+        } else {
+            storeTwo += value;
+            console.log(`La seconde valeur selectionnée est ${storeTwo}`);
+            displaySum.innerText = storeTwo;
+
+        };
     });
 });
-
-
-
-// Créer une varialbe pour sélectionner le bouton afin de l'appeler avec un eventListener
-// const buttons = document.querySelectorAll("button");
-// let storeOne = "";
-// let storeTwo = "";
-// let isOperatorSelected = false;
-// let isSecondOperatorSelected = false;
-
-
-// buttons.forEach((bouton) => {
-//     bouton.addEventListener("click", () => {
-
-//         const value = bouton.innerText;
-
-//         if (value === "=") {
-//             if (storeOne && storeTwo && isOperatorSelected) {
-//                 a = parseFloat(storeOne);
-//                 b = parseFloat(storeTwo);
-//                 const result = operate(a, b, operator);
-//                 console.log(`Le résultat est ${result}`)
-//             } else {
-//                 console.log("Erreur, veuillez refaire votre opération")
-//             }
-//         } else if (value === "Clear") {
-//             storeOne = "";
-//             storeTwo = "";
-//             isOperatorSelected = false;
-//             console.log("Reset ok, tout est bien à 0")
-//         } else if (value === "+" || value === "-" || value === "*" || value === "/") {
-//             isOperatorSelected = true;
-//             operator = value;
-//             console.log(`Operator selected is ${operator}`)
-//         } else if (!isOperatorSelected) {
-//             storeOne += value;
-//             console.log(`La première valeur selectionnée est ${storeOne}`)
-//         } else {
-//             storeTwo += value;
-//             console.log(`La seconde valeur selectionnée est ${storeTwo}`)
-//         }
-//     })
-// })
-
